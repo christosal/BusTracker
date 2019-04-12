07bec6273c23a6c52ca43a1f91d03ef307cb573b
Christos <christian.salias@gmail.com>
2019-04-12 04:23:54 +0300

    master

---
+++ src/Subscriber.java
@@ -0,0 +1,14 @@
+public class Subscriber {
+
+    public void register(Broker broker,Topic topic){
+
+    }
+
+    public void disconnect(Broker broker){
+
+    }
+
+    public void visualizeData(Topic topic,Value value){
+
+    }
+}
